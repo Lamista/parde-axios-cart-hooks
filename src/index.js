265f997ab095acbc758f9e458180c1e94a0adc12
Laura <laura.stasiule@gmail.com>
2020-12-03 19:11:38 +0200

    push

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ document.title = "E-shop";
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <App />
     </BrowserRouter>
   </React.StrictMode >,
